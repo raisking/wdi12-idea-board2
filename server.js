909b964900f031ebc8f842416090393ff0248b67
raisking <raiprakas007@gmail.com>
2017-10-12 11:19:13 -0400

    Install , Added  concurrently and react

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-//Imports variables from .env file
+// Imports variables from .env file
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 mongoose.Promise = global.Promise
-//Create a new app using express
+// Create a new app using express
 const app = express()
-//Connect to MongoDB and set up messages for when
-//Mongo connects successfully or error out 
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
-const connection = mongoose.connection;
+
+// Connect to MongoDB and set up messages for when 
+// Mongo connects successfully or errors out
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+const connection = mongoose.connection
 
 connection.on('connected', () => {
-    console.log('Successfully connected to MongoDB')
+  console.log('Successfully connected to MongoDB')
 })
 
 connection.on('error', (err) => {
-    console.log('MongoDB Error:', err)
+  console.log('MongoDB Error: ', err)
 })
-//inject middleware 
+
+// Inject middleware
 app.use(bodyParser.json())
 
-app.get('/', (req, res) =>{
-    res.send("Hello World")
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=> {
-    console.log("App listening on port: ", PORT)
-}) 
+// Set the app to listen on a specific port
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log('App listening on port: ', PORT)
+})
